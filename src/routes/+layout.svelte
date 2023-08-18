bf0b5fe6f301677fc65d814aaf91c849a7733967
= <=>
2023-08-17 22:18:12 -0500

    chore: update layout imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../app.postcss'
-	import Header from '../components/header.svelte'
-	import Footer from '../components/footer.svelte'
+	import Header from '../components/Header.svelte'
+	import Footer from '../components/Footer.svelte'
 </script>
 
 <div class='container mx-auto sm:border-x sm:border-x-gray-100 max-w-7xl'>
