786584c3a6badedc49b6f53b50478b81ccc5503b
= <=>
2023-09-17 10:12:41 -0500

    chore: clean up queries

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,68 +1,5 @@
 import { gql } from 'graphql-request'
 
-export const GET_SECTIONS = gql`
-  query getSections($id: ID!) {
-    bibleStudy(where: { id: $id }) {
-      sections {
-        id
-        modules {
-          ... on Introduction {
-            id
-          }
-          ... on Question {
-            id
-          }
-        }
-      }
-    }
-  }
-`
-
-export const GET_SECTION = gql`
-  query getSection($id: ID!) {
-    section(where: { id: $id }) {
-      modules(first: 50) {
-        ... on Introduction {
-          id
-          title
-          text {
-            text
-          }
-        }
-        ... on Question {
-          id
-          verses(first: 5) {
-            ... on Verse {
-              verse
-              text {
-                text
-              }
-            }
-          }
-          connectingVerses(first: 50) {
-            ... on Verse {
-              verse
-              text {
-                text
-              }
-            }
-          }
-          commentaries(first: 5) {
-            text {
-              text
-            }
-            author
-            reference
-          }
-          note {
-            text
-          }
-        }
-      }
-    }
-  }
-`
-
 export const GET_BIBLE_STUDIES = gql`
   {
     bibleStudies {
@@ -72,18 +9,12 @@ export const GET_BIBLE_STUDIES = gql`
       image {
         url
       }
-      sections {
-        id
-        modules {
-          __typename
-        }
-      }
     }
   }
 `
 
 export const GET_BIBLE_STUDY = gql`
-  query getBibleStudy($id: ID) {
+  query getBibleStudyByID($id: ID) {
     bibleStudy(where: { id: $id }) {
       id
       title
@@ -96,46 +27,19 @@ export const GET_BIBLE_STUDY = gql`
       sections {
         id
         title
-        modules {
-          ... on Introduction {
-            __typename
-            id
-            title
-            text {
-              text
-            }
+        modules_v2 {
+          id
+          title
+          order
+          isCriticalthinking
+          introduction {
+            text
           }
-          ... on Question {
-            __typename
-            id
-            type
-            title
-            question
-            answer
-            verses(first: 5) {
-              ... on Verse {
-                verse
-                text {
-                  text
-                }
-              }
-            }
-            connectingVerses(first: 5) {
-              ... on Verse {
-                verse
-                text {
-                  text
-                }
-              }
-            }
-            commentaries(first: 5) {
-              text {
-                text
-              }
-              author
-              reference
-            }
-            note {
+          question
+          answer
+          verses {
+            reference
+            text {
               text
             }
           }
