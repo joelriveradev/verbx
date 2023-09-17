5f0a2cccc60d5e886eec28e04a61c8f14339f5de
= <=>
2023-09-17 09:47:37 -0500

    chore: clean up

--- src/components/CourseSection.svelte
+++ src/components/CourseSection.svelte
@@ -1,18 +1,15 @@
 <script lang='ts'>
   import type { Section } from '../types/typegen/graphql'
+
+  export let section: Section
   export let open = false
   export let activeModule: string
-  export let complete: boolean = false
-
-  $: expanded = open
-  const toggleActive = () => expanded = !expanded
-
+  export let sectionCompleted: boolean = false
   export let padding = false
-  export let section: Section
   export let showComplete = false
   export let handleModuleClick: (id: string) => void
 
-  const { title } = section
+  const toggleActive = () => open = !open
 </script>
 
 <div class='course-section'>
@@ -24,36 +21,42 @@
     role='button'
     tabindex="-1">
 
-    <span class='font-light'>{title}</span>
+    <span class='font-light'>
+      {section.title}
+    </span>
 
     <img
-      class={expanded ? 'w-4 rotate-180 transition-transform' : 'w-4 transition-transform'}
+      class={open ? 'w-4 rotate-180 transition-transform' : 'w-4 transition-transform'}
       src='/carrot-down-xs.svg'
       alt=''
       aria-hidden
     />
   </div>
 
-  {#if expanded}
+  {#if open}
     <div class='pt-5 pr-10' class:pl-10={padding}>
       {#each section.modules_v2 as { id, title }, index }
         {@const active = activeModule === id }
         {@const activeIndex = section.modules_v2.findIndex(m => m.id === activeModule)}
-        {@const completed = index < activeIndex}
+        {@const moduleCompleted = index < activeIndex}
+        {@const enabled = active || moduleCompleted || sectionCompleted}
         <!-- svelte-ignore a11y-no-static-element-interactions -->
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div class='flex items-center mb-5 cursor-pointer' on:click={() => handleModuleClick(id)}>
-          {#if showComplete && completed || showComplete && complete}
+
+          {#if showComplete && moduleCompleted || showComplete && sectionCompleted}
             <img class='w-2.5 mr-3' src='/check-xs.svg' alt='' />
-            {:else if !completed && !active}
-              <img class='w-2.5 mr-3 opacity-30' src='/lock.svg' alt='' />
-            {:else}
-              <div class='w-3 h-3 rounded-full mr-3 border border-gray-400' />
+
+          {:else if !moduleCompleted && !active}
+            <img class='w-2.5 mr-3 opacity-30' src='/lock.svg' alt='' />
+
+          {:else}
+            <div class='w-3 h-3 rounded-full mr-3 border border-gray-400' />
           {/if}
 
           <p
-            class={active || complete ? 'text-black text-sm font-light' : 'font-light text-gray-500 text-sm'}
-            class:font-medium={active && !complete}
+            class={enabled ? 'text-black text-sm font-light' : 'font-light text-gray-500 text-sm'}
+            class:font-medium={active && !sectionCompleted}
           >
             {title}
           </p>
