8a7c7a94d79121b2d16a48564825279753a05e33
= <=>
2023-10-02 09:08:49 -0500

    chore: update imports for main page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,8 @@
+import type { CoursesResponse } from '../types'
+
 import { GraphQLClient } from 'graphql-request'
 import { GET_BIBLE_STUDIES } from '../queries'
 import { HYGRAPH_API_URL_HIGHPERF } from '$env/static/private'
-
-import type { CoursesResponse } from '../types'
-import type { BibleStudy } from '../types/typegen/graphql'
 
 export const load = async () => {
   const hygraph = new GraphQLClient(HYGRAPH_API_URL_HIGHPERF)
